d9a2918be84d5e69267750fd508d7efbdb9a076c
VPivotto <68520601+VPivotto@users.noreply.github.com>
2023-05-23 20:06:47 -0300

    Correcciones

--- scripts/FACET - PALERMO/chart.js
+++ scripts/FACET - PALERMO/chart.js
@@ -3,8 +3,8 @@ d3.json('https://cdn.jsdelivr.net/npm/d3-time-format@3/locale/es-ES.json').then(
   d3.timeFormatDefaultLocale(locale)
 })
 
-const mapaFetch = d3.json('../../data/palermo.geojson');
-const dataFetch = d3.dsv(';', '../../data/147_vehiculos_mal_estacionados.csv', d3.autoType);
+const mapaFetch = d3.json('palermo.geojson');
+const dataFetch = d3.dsv(';', '147_vehiculos_mal_estacionados.csv', d3.autoType);
 
 Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
   const datosFiltrados = data.filter((d) => d.prestacion === 'VEHÍCULO MAL ESTACIONADO' && d.domicilio_barrio === 'PALERMO');
@@ -17,10 +17,9 @@ Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
   
   /* Mapa Coroplético */
   let chartMap = Plot.plot({
-    // https://github.com/observablehq/plot#projection-options
     projection: {
       type: 'mercator',
-      domain: barrios, // Objeto GeoJson a encuadrar
+      domain: barrios,
     },
     color: {
       scheme: 'ylorbr',
@@ -31,26 +30,39 @@ Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
         y: 'lat',
         fill: 'density',
         bandwidth: 1,
-        thresholds: 30,
+        thresholds: 1000,
       }),
       Plot.geo(barrios, {
         stroke: 'grey',
         fill: 'transparent',
         title: (d) => `${d.properties.BARRIO}\n${d.properties.DENUNCIAS} denuncias`,
       }),
-      
+      Plot.text(
+        [{lon: -58.445, lat: -34.577}],
+        {
+          x: 'lon',
+          y: 'lat',
+          text: (d) => "Plaza Dumont",
+          fill: "black",
+          stroke: "white",
+          textAnchor: "center",
+          dx: 4,
+          fontSize: 10,
+        }
+        ),
     ],
     facet: {
       data: datosFiltrados,
-      x: d => d3.timeFormat('%a')(d3.timeParse('%d/%m/%Y')(d.fecha_ingreso)),
+      x: d => {
+        const dayOfWeek = d3.timeParse('%d/%m/%Y')(d.fecha_ingreso).getDay();
+        return dayOfWeek >= 1 && dayOfWeek <= 5 ? 'Lunes-Viernes' :  'Sábado-Domingo';
+      },
     },
     fx: {
-      domain: ['lun', 'mar', 'mié', 'jue', 'vie', 'sáb', 'dom']
+      domain: ['Lunes-Viernes', 'Sábado-Domingo']
     },
     width: 1000
   })
 
-  /* Agregamos al DOM la visualización chartMap */
   d3.select('#chart').append(() => chartMap)
 })
-
